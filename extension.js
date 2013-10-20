5591ee15dbd8b2c20e3860571a804f6f88ee9946
green <green@gPC.(none)>
2013-10-20 19:07:37 +0100

    first start icon fix

--- extension.js
+++ extension.js
@@ -288,12 +288,14 @@ TaskBar.prototype =
     //First Start
     firstStart: function()
     {
+        if this.settings.get_string("desktop-button-icon" === "default")
+            this.settings.set_string("desktop-button-icon", DESKTOPICON);
+        if this.settings.get_string("appview-button-icon" === "default")
+            this.settings.set_string("appview-button-icon", APPVIEWICON);
         if (ShellVersion[1] === 4)
         {
             if (this.settings.get_boolean("first-start"))
             {
-                this.settings.set_string("desktop-button-icon", DESKTOPICON);
-                this.settings.set_string("appview-button-icon", APPVIEWICON);
                 Main.Util.trySpawnCommandLine('gnome-shell-extension-prefs ' + Extension.metadata.uuid);
                 this.settings.set_boolean("first-start", false);
             }
@@ -302,8 +304,6 @@ TaskBar.prototype =
         {
             if ((this.settings.get_boolean("first-start")) && (Main.sessionMode.currentMode == 'user'))
             {
-                this.settings.set_string("desktop-button-icon", DESKTOPICON);
-                this.settings.set_string("appview-button-icon", APPVIEWICON);
                 Main.Util.trySpawnCommandLine('gnome-shell-extension-prefs ' + Extension.metadata.uuid);
                 this.settings.set_boolean("first-start", false);
             }
