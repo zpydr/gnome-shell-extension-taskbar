e41a84516b4d05385f3725ccbd956e43367e22ae
zpydr <zpydr@openmailbox.org>
2017-06-05 21:32:43 +0100

    bottom panel extension support

--- scripts/import.sh
+++ scripts/import.sh
@@ -3,5 +3,6 @@ if [ ! -f $HOME/taskbar.dconf ]; then
     zenity --info --text 'Import of TaskBar Settings Failed!'
 else
     dconf load /org/gnome/shell/extensions/TaskBar/ <$HOME/taskbar.dconf
-    zenity --info --text 'Import of TaskBar Settings Successful!'
+    killall gnome-shell-extension-prefs
+    zenity --info --text 'Import of TaskBar Settings Successful!' && gnome-shell-extension-prefs TaskBar@zpydr
 fi
