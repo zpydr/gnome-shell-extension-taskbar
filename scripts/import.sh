9b4a418da2de9a0a5f7a373d3bf690455498bcc4
zpydr <zpydr@openmailbox.org>
2017-06-06 20:39:11 +0100

    gnu gpl

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,3 +1,23 @@
+#  GNOME Shell Extension TaskBar
+#  Copyright (C) 2013-2017 zpydr
+#
+#  Version 55
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https:#www.gnu.org/licenses/>.
+#
+#  zpydr@openmailbox.org
+#
 #!/bin/bash
 if [ ! -f $HOME/taskbar.dconf ]; then
     zenity --info --text 'Import of TaskBar Settings Failed!'
